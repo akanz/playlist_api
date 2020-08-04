79c7bd643160cda242b6a342b9cb96696dc055d5
Manakanz <samuelakande2014@yahoo.com>
2020-08-04 22:01:23 +0100

    documentation

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,15 +1,8 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
-});
- $('.navTrigger').click(function () {
-$(this).toggleClass('active');
-console.log("Clicked menu");
-$("#mainListDiv").toggleClass("show_list");
-$("#mainListDiv").fadeIn();
+function openSideMenu(){
+    document.getElementById('side-menu').style.width = '60vw';
+  }
 
-});
+  function closeSideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+  }
+  
